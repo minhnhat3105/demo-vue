bd27649babc34238c70e60d21ae7936b18cbce7c
daddy <minhnhat3105@gmail.com>
2021-08-29 08:36:11 +0700

    master

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyDWBNtqXCnG-zL-toKdPZ1AnLZDJVp9lg0",
+  authDomain: "vuefs-project-190b1.firebaseapp.com",
+  projectId: "vuefs-project-190b1",
+  storageBucket: "vuefs-project-190b1.appspot.com",
+  messagingSenderId: "89308807617",
+  appId: "1:89308807617:web:fdffbd5a3ae4636fd32f40",
+  measurementId: "G-SHWLEL3WTC"
+};
---
+++ src/router/index.js
@@ -0,0 +1,40 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Dashboard from '@/components/Dashboard'
+import NewEmployee from '@/components/NewEmployee'
+import ViewEmployee from '@/components/ViewEmployee'
+import EditEmployee from '@/components/EditEmployee'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'dashboard',
+    component: Dashboard
+  },
+  {
+    path: '/new',
+    name: 'new-employee',
+    component: NewEmployee
+  },
+  {
+    path: '/edit/:employee_id',
+    name: 'edit-employee',
+    component: EditEmployee
+  },
+  {
+    path: '/:employee_id',
+    name: 'view-employee',
+    component: ViewEmployee
+  }
+  
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
