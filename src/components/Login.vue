d0c9466a509b8d9c486218424a823a56529652c5
daddy <minhnhat3105@gmail.com>
2021-09-26 22:11:59 +0700

    first-commit

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,32 +1,38 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col s12 m8 offset-m2">
-                <div class="login card-panel green white-text center">
-                    <h3>Login</h3>
-                    <form action="" method="POST">
-                        <div class="input-field">
-                            <i class="material-icons prefix">email</i>
-                            <input type="text" id="email" v-model='email'>
-                            <label class="white-text" for="email">Email</label>
-                        </div>
-                        <div class="input-field">
-                            <i class="material-icons prefix">lock</i>
-                            <input type="password" id="password" v-model='password'>
-                            <label class="white-text" for="password">Password</label>
-                        </div>
-                        <button @click='login' type="submit" class="btn btn-large grey lighten-4 black-text">Login</button>
-                    </form>
-                </div>
+  <div class="container">
+    <div class="row">
+      <div class="col s12 m8 offset-m2">
+        <div class="login card-panel green white-text center">
+          <h3>Login</h3>
+          <form action="" method="POST">
+            <div class="input-field">
+              <i class="material-icons prefix">email</i>
+              <input type="text" id="email" v-model="email" />
+              <label class="white-text" for="email">Email</label>
             </div>
+            <div class="input-field">
+              <i class="material-icons prefix">lock</i>
+              <input type="password" id="password" v-model="password" />
+              <label class="white-text" for="password">Password</label>
+            </div>
+            <button
+              @click="login"
+              type="submit"
+              class="btn btn-large grey lighten-4 black-text"
+            >
+              Login
+            </button>
+          </form>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 <script>
-import firebase from 'firebase'
-export default{
-    name: 'login',
-    data() {
+import firebase from "firebase";
+export default {
+  name: "login",
+  data() {
     return {
       email: "",
       password: "",
@@ -40,7 +46,7 @@ export default{
         .then(
           (user) => {
             alert(`You are logged in as ${user.email}`); // `: get value of variable
-            this.$router.go({path: this.$router.path});
+            this.$router.go({ path: this.$router.path });
           },
           (err) => {
             alert(err.message);
@@ -49,5 +55,5 @@ export default{
       e.preventDefault();
     },
   },
-}
+};
 </script>
